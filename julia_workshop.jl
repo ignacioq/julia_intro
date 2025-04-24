05d0b16914d23fb6a0f39175743c4810bde0eac2
Ignacio Quintero <ignacioquintero@gmail.com>
2025-04-24 17:30:07 +0200

    updated workshopo

--- julia_workshop.jl
+++ julia_workshop.jl
@@ -317,6 +317,7 @@ typeof(shcom)
 # use run to evaluate
 run(shcom)
 
+
 # install any package with Pkg.add("PackageName")
 
 ## Integrating with R
@@ -399,7 +400,9 @@ x[[2,3]]
 x[2:3]
 typeof(2:3)
 
-# length and lastindex return the length
+#= length and lastindex return the length (lastindex do not work with 
+un-indexed collections such as Dictionaries but is slightly faster)
+=#
 lastindex(x) == length(x)
 
 # `end` returns the last item
@@ -444,12 +447,13 @@ v = collect(ur)
 v[10] = 5
 
 # StepRange construction is `start:step:end`
-# Int
 sr = 1:2:6
 typeof(sr)
 
-# you can construct with `range` it estimates the 
-# StepRange according to the number of objects and the step size
+#=
+you can construct with `range`: it estimates the 
+StepRange according to the number of objects and the step size
+=#
 1:2:6 == range(1, step=2, length=3)
 
 # also works with Floats
@@ -487,7 +491,7 @@ isa(u, Vector{Vector{Vector{Float64}}})
 ## Functions on vectors
 # fuse two vectors
 x = [1:3...]
-append!(x,4:5)
+append!(x, 4:5)
 x
 
 """
@@ -746,7 +750,7 @@ rand(S)
 # you can create a random string with
 randstring(10)
 
-# check that it is uniform pick (1/3 for 7)
+# check that it is uniform pick (1/3 for 4)
 x = [rand(S) for i in 1:100_000]
 
 using Statistics
@@ -773,7 +777,7 @@ v = randperm(10)
 shuffle!(v)
 
 ## basic statistics in library `Statistics`
-x = randn(10_000)
+x = randn(10000)
 
 mean(x)
 std(x)
@@ -785,6 +789,7 @@ prod(x)
 # these functions usually allow to provide a function
 # before aggregating
 mean(exp, x)
+mean(exp.(x))
 
 # Pearson correlation and covariance
 y = randn(10_000) .* 0.1
@@ -959,14 +964,16 @@ Scoping rules are relatively intuitive, but since Julia >= 1.5,
 loops do not allow modifying global variables except when in the REPL.
 """
 
-# for instance, this work but is not recommended
+# for instance, this works but is not recommended
 j = 0
 for i in 1:5
   j += i
 end
-# because the loop does not search for variables in the global scope.
-# This is for efficiency (avoiding global variable definitions is, 
-# overall, better coding) and follows "good" coding standards.
+#=
+because the loop does not search for variables in the global scope.
+This is for efficiency (avoiding global variable definitions is, 
+overall, better coding) and follows "good" coding standards.
+=#
 
 # However, loops do allow modification to the parent scope as long as is local:
 for j in 1:2
@@ -977,8 +984,10 @@ for j in 1:2
   end
 end
 
-#To allow the efficient modification of global variables there are two options, 
-# surround in a `let` block, such like
+#=
+To allow the efficient modification of global variables there are two options, 
+surround in a `let` block, such like
+=#
 let j = 0
   for i in 1:5
     j += i
@@ -986,8 +995,10 @@ let j = 0
   println(j)
 end
 
-# or declare that the variable is actually global, and now the loop
-# will look for global variables.
+#=
+or declare that the variable is actually global, and now the loop
+will look for global variables.
+=#
 j = 0
 for i in 1:5
   global j += i
@@ -1214,7 +1225,7 @@ using BenchmarkTools
 @benchmark fbad(10)
 @benchmark fgood(10, 10)
 
-# scape parts to not be included in the performance evaluation with `$`
+# `scaped` parts to not be included in the performance evaluation with `$`
 f(x) = sum(x)
 @benchmark f([1,2,3,4,5])
 @benchmark f($[1,2,3,4,5])
@@ -1240,6 +1251,7 @@ reval("""
 logdnorm(x::Float64, μ::Float64, σ²::Float64) = 
   -(0.5*log(2.0π) + 0.5*log(σ²) + (x - μ)^2/(2.0σ²))
 
+
 # define generator for non-standard normal
 rnorm(μ::Float64, σ::Float64) = μ + randn()*σ
 
@@ -1263,8 +1275,6 @@ map(cos, 0:π/4:2π)
 # for more than one argument, use a tuple
 map((x,y) -> x + y, [1:10...], [2:11...])
 
-# can also be empty (here, of course, is useless)
-map(() -> rand(0:1,10))
 
 
 ### Multiple dispatch
@@ -1304,7 +1314,7 @@ sum_same_type(1, 1)
 sum_same_type(1.0, 1.0)
 
 # but throws an error not if they are of different types
-sum_same_type(1.0,1)
+sum_same_type(1.0, 1)
 
 # we might want to restrict the type to be numeric since...
 sum_same_type("1","1")
@@ -1363,7 +1373,7 @@ end
 elem_sum([1,2,3],[1,2,3])
 
 # for matrices
-elem_sum(rand(10,10),rand(10,10))
+elem_sum(rand(10,10),zeros(10,10))
 
 # multiple dispatch with values
 function fval(x::Int64, ::Val{1})
@@ -1629,6 +1639,8 @@ t = randn(2,10)
 @load "/Users/quintero/repos/julia_intro/work.jld2"
 
 
+# let's remove those files from the repo
+rm("/Users/quintero/repos/julia_intro/work.jld2")
 
 #=
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -1675,340 +1687,6 @@ end
 """
 Exercises 03
 """
-
-
-#=
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-A tiny bit of Meta Programming
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-=#
-
-# All code in Julia is represented as Julia code structures itself
-
-# `Expr` are code objects that are not evaluated
-x = :(2.0 + 2.0)
-typeof(ans)
-
-# `eval()` allows to evaluate Expr
-eval(x)
-
-# there is a hierarchical tree of evaluation
-x = :(2 + 2*3)
-
-# the filed `args` allows us to access the hierarchy of evaluation
-x.args
-
-# we can change any element from this evaluation
-# say, change the sum for a multiplication
-x.args[1] = :*
-
-# now the Expr uses product
-eval(x)
-
-# parse returns an Expr object from a string
-x  = "1 + 2^3"
-px = Meta.parse(x)
-eval(px)
-
-
-#=
-let's look at a more advanced example with `@generated` functions
-from actual useful functions
-
-Let's create an simple approximating function from an ordered vector x, and 
-a function y = f(x).
-=#
-
-## Let's create some random data to create a function y = f(x)
-# `x` is a sorted vector (e.g., time)
-x = cumsum(rand(100))
-x[1] = 0.0
-
-# `y` is 100 `parallel` multivariate functions under Brownian motion
-y = randn(100, 100)
-cumsum!(y, y, dims = 1)
-
-
-## First we need a linear interpolation function
-"""
-    linpred(val::Float64, x1::Float64, x2::Float64, y1::Float64, y2::Float64)
-
-Estimate `val` according to linear interpolation for a range.
-"""
-linpred(val::Float64, x1::Float64, x2::Float64, y1::Float64, y2::Float64) = 
-  (y1 + (val - x1)*(y2 - y1)/(x2 - x1))
-
-
-# example
-val = x[2] - rand()*x[2]
-linpred(val, x[1], x[2], y[1,1], y[2,1])
-
-## Second create an efficient index searcher for a sorted vector
-
-"""
-    idxrange(x::Array{Float64,1}, val::Float64)
-
-Get indexes in sorted vector `x` corresponding to the range in which 
-`val` is in using a sort of uniroot algorithm.
-"""
-function idxrange(x::Array{Float64,1}, val::Float64)
-
-  @inbounds begin
-
-    a::Int64 = 1
-
-    if x[a] > val
-      return a, false
-    end
-
-    b::Int64 = lastindex(x)
-
-    if x[b] < val
-      return b, false
-    end
-
-    mid::Int64 = div(b,2)
-
-    while b-a > 1
-      val < x[mid] ? b = mid : a = mid
-      mid = div(b + a, 2)
-    end
-
-    if x[a] == val 
-      return a, false
-    elseif x[b] == val
-      return b, false
-    else
-      return a, true
-    end
-
-  end
-end
-
-
-# example: returns the closer left index in x to the value
-idx, inside = idxrange(x, 20.)
-x[idx]
-x[idx+1]
-
-# this is efficient
-@benchmark idxrange($x, 22.)
-
-# we can check that it is type stable
-@code_warntype idxrange(x, 19.)
-
-
-## Now let's make a meta program that does not loop through each of the 
-# y columns
-
-# We need to define some arguments that will go into the function (see later)
-N  = ndims(y)
-nc = size(y,2)
-
-# start the Expr
-lex1 = quote end
-pop!(lex1.args)
-
-# push the linear prediction and unroll
-if isone(N)
-  push!(lex1.args, 
-    :(r[1] = linpred(t, x[a], x[a+1], y[a], y[a+1])::Float64))
-else
-  # unroll loop
-  for i = Base.OneTo(nc)
-    push!(lex1.args, 
-      :(r[$i] = linpred(t, xa, xap1, y[a,$i], y[a+1, $i])::Float64))
-  end
-
-  # add one assignment
-  pushfirst!(lex1.args, :(xap1 = x[a+1]::Float64))
-  pushfirst!(lex1.args, :(xa   = x[a]::Float64))
-end
-
-lex2 = quote end
-pop!(lex2.args)
-
-# if the value is outside of `x` 
-if isone(N)
-  push!(lex2.args, :(r[1] = y[a]::Float64))
-else
-  # unroll loop
-  for i = Base.OneTo(nc)
-    push!(lex2.args, :(r[$i] = y[a,$i]::Float64))
-  end
-end
-
-# join both expressions
-lex = quote
-  a, lp = idxrange(x, t)::Tuple{Int64, Bool}
-  if lp 
-    $lex1 
-  else 
-    $lex2 
-  end
-end
-
-# aesthetic cleaning
-deleteat!(lex.args,[1,3])
-
-popfirst!(lex.args[2].args[2].args)
-lex.args[2].args[2] = lex.args[2].args[2].args[1]
-
-popfirst!(lex.args[2].args[3].args)
-lex.args[2].args[3] = lex.args[2].args[3].args[1]
-
-# let's see the final function to be evaluated
-lex
-
-
-#=
-Now let's put it in a `@generated` function. This special awesome function
-will first  generate the code, and then will only evaluate the Expression.
-=#
-
-"""
-    @generated function approxf_full(t ::Float64,
-                                     r ::Array{Float64,1},
-                                     x ::Array{Float64,1}, 
-                                     y ::Array{Float64,N},
-                                     ::Val{nc}) where {N, nc}
-Returns the values of `y` at `t` using an approximation function.
-"""
-@generated function approxf_full!(t ::Float64,
-                                  r ::Array{Float64,1},
-                                  x ::Array{Float64,1}, 
-                                  y ::Array{Float64,N},
-                                  ::Val{nc}) where {N, nc}
-
-  lex1 = quote end
-  pop!(lex1.args)
-
-  if isone(N)
-    push!(lex1.args, 
-      :(r[1] = linpred(t, x[a], x[a+1], y[a], y[a+1])::Float64))
-  else
-    # unroll loop
-    for i = Base.OneTo(nc)
-      push!(lex1.args, 
-        :(r[$i] = linpred(t, xa, xap1, y[a,$i], y[a+1, $i])::Float64))
-    end
-
-    # add one assignment
-    pushfirst!(lex1.args, :(xap1 = x[a+1]::Float64))
-    pushfirst!(lex1.args, :(xa   = x[a]::Float64))
-  end
-
-  lex2 = quote end
-  pop!(lex2.args)
-
-  if isone(N)
-    push!(lex2.args, :(r[1] = y[a]::Float64))
-  else
-    # unroll loop
-    for i = Base.OneTo(nc)
-      push!(lex2.args, :(r[$i] = y[a,$i]::Float64))
-    end
-  end
-
-  lex = quote
-    a, lp = idxrange(x, t)::Tuple{Int64, Bool}
-    if lp 
-      $lex1 
-    else 
-      $lex2 
-    end
-  end
-
-  # aesthetic cleaning
-  deleteat!(lex.args,[1,3])
-
-  popfirst!(lex.args[2].args[2].args)
-  lex.args[2].args[2] = lex.args[2].args[2].args[1]
-
-  popfirst!(lex.args[2].args[3].args)
-  lex.args[2].args[3] = lex.args[2].args[3].args[1]
-
-  println(lex)
-
-  return quote
-    @inbounds begin
-      $lex
-    end
-    return nothing
-  end
-end
-
-
-# example
-r = Array{Float64,1}(undef,size(y,2)) # preallocate resulting interpolation
-approxf_full!(5., r, x, y, Val(size(y,2)))
-r
-
-# compare to a non generated function
-function approxf_full_std!(t ::Float64,
-                           r ::Array{Float64,1},
-                           x ::Array{Float64,1}, 
-                           y ::Array{Float64,N}) where {N}
-
-  @inbounds begin
-    a, lp = idxrange(x, t)::Tuple{Int64, Bool}
-
-    if lp
-      xa = x[a]::Float64
-      xap1 = x[a + 1]::Float64
-      for i = Base.OneTo(size(y,2))
-        r[i] = linpred(t, xa, xap1, y[a, i], y[a + 1, i])::Float64
-      end
-    else
-      for i = Base.OneTo(size(y,2))
-        r[i] = y[a, i]::Float64
-      end
-    end
-  end
-
-  return nothing
-end
-
-@benchmark approxf_full!(5., $r, $x, $y, $(Val(size(y,2))))
-@benchmark approxf_full_std!(5., $r, $x, $y)
-
-
-
-# lets profile to check for any bottlenecks
-using Profile, ProfileView
-
-Profile.clear()
-
-@profile for i in 1:1_000_000 approxf_full_std!(20., r, x, y) end
-
-Profile.print()
-ProfileView.view()
-
-
-#=
-Finally, let's compare to a similar R function that is implemented in C 
-(not super-fair comparison for our function is very tailored made, but still...)
-=#
-@rput x
-@rput y
-
-reval("""
-  require(microbenchmark)
-
-  f = approxfun(x, y[,1])
-  r1 = f(5.0) # evaluate at 5.0
-
-  microbenchmark(f(5.0))
-      """)
-
-# check that we get the same answer
-@rget r1
-
-r1    # R results
-r[1]  # Julia result
-# same answer but WAY faster!
-
-
 
 
 
